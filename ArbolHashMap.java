69e371d7e4ae85911780f2f3cf78a5aef0770309
mac2218 <mac22118@uvg.edu.gt>
2023-04-21 21:45:14 -0600

    Create ArbolHashMap.java

---
+++ ArbolHashMap.java
@@ -0,0 +1,48 @@
+/*
+ * Daniel Machic 
+ * HT #9
+ * 21/04/2023
+ */
+
+import java.util.HashMap;
+
+public class ArbolHashMap<K, T> implements IEstructuraArbol<T> {
+
+	HashMap<K, T> miMapaInterno;
+	IKeyGenerator<K, T> internalKeyCollector;
+	
+	public ArbolHashMap(IKeyGenerator<K, T> keyCollector) {
+		miMapaInterno = new HashMap<K, T>();
+		internalKeyCollector = keyCollector;
+	}
+	
+	
+	@Override
+	public void add(T value) {
+		K key = internalKeyCollector.getKey(value);
+		miMapaInterno.put(key, value);
+	}
+
+	@Override
+	public T get(T key) {
+		K _key = internalKeyCollector.getKey(key);
+		return miMapaInterno.get(_key);
+	}
+
+	@Override
+	public T remove(T key) {
+		K _key = internalKeyCollector.getKey(key);
+		return miMapaInterno.remove(_key);
+	}
+
+	@Override
+	public int count() {
+		return miMapaInterno.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return miMapaInterno.isEmpty();
+	}
+
+}
